80792cb107160373fc36f6b65d52de7a279c9e10
rksam <erksg.10.26@gmail.com>
2025-03-10 19:06:35 -0500

    add pj Back

---
+++ PjCobranza-Back/src/main/java/com/example/demo/PjCobranzaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjCobranzaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjCobranzaBackApplication.class, args);
+	}
+
+}
